3058a21a033cc4d9b426b4efa1d2d25fda612b37
Hoàng Anh Nguyễn <nganhh014@gmail.com>
2021-08-17 20:49:56 +0300

    upload the solution

--- part05-e11_mystery_data/src/mystery_data.py
+++ part05-e11_mystery_data/src/mystery_data.py
@@ -4,11 +4,16 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 def mystery_data():
-    return []
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    model = LinearRegression(fit_intercept=False)
+    model.fit(df.iloc[:,0:5], df.iloc[:,5])
+    return model.coef_
 
 def main():
     coefficients = mystery_data()
     # print the coefficients here
-    
+    for i, co in enumerate(coefficients):
+        print("Coefficient of X{} is {}".format(i+1, co))
+        
 if __name__ == "__main__":
     main()
